a02629c64ec6b138e1d936e1608a285de4aa7c67
Jon Hedegaard Nielsen <jon230706@gmail.com>
2026-02-16 13:11:40 +0100

    returned to old status

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import { useEffect } from 'react'
 import './App.css'
-import { BrowserRouter } from 'react-router'
-import { Routes } from 'react-router'
-import { Route } from 'react-router'
-import { MainLayout } from './layouts/MainLayout'
-import { About } from './pages/About/About'
-import { Home } from './pages/Home/Home'
-import { Contact } from './pages/Contact/Contact'
-import { Products } from './pages/Products/Products'
+import { Counter } from './components/Counter/Counter';
+import { TitleSetter } from './components/TitleSetter/TitleSetter';
+import { Toggle } from './components/Toggle/Toggle';
 
 function App() {
 	useEffect(() => console.log("Velkommen Til Min Side"), []);
 	return (
 		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<MainLayout />}>
-						<Route path='/about' element={<About />} />
-						<Route path='/products' element={<Products />} />
-						<Route path='/home' element={<Home />} />
-						<Route path='/contact' element={<Contact />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<Counter />
+			<TitleSetter />
+			<Toggle />
 		</>
 	)
 }
--- src/pages/About/About.tsx
+++
@@ -1,9 +0,0 @@
-import './About.css';
-export const About = () => {
-	return (
-		<div className="about">
-			<h3>Dette er en cool side</h3>
-		</div>
-	)
-}
-
